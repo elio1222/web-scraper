ad5bff12605fb5a32c6e6e1b8d4e3248566c373f
elio1222 <124097037+elio1222@users.noreply.github.com>
2025-11-03 22:40:02 -0500

    automation at its finest.

--- cua.py
+++ cua.py
@@ -11,11 +11,59 @@ from bs4 import BeautifulSoup
 
 def wait():
     time.sleep(1)
+
+def loop_through_subjects(sub):
+    wait()
+    WebDriverWait(driver, 5).until(
+    EC.presence_of_all_elements_located((By.ID, 'subject'))
+    )
+    subject = Select(driver.find_element(By.ID, 'subject'))
+    subject.select_by_visible_text(f'{sub}')
+
+    wait()
+    WebDriverWait(driver, 5).until(
+        EC.element_to_be_clickable((By.ID, 'view_catalog'))
+    )
+    input = driver.find_element(By.ID, 'view_catalog')
+    input.click()
+
+    wait()
+    WebDriverWait(driver, 5).until(
+        EC.element_to_be_clickable((By.ID, 'courseSchedExpandAll'))
+    )
+    expand = driver.find_element(By.ID, 'courseSchedExpandAll')
+    expand.click()
+
+    wait()
+    class_titles = driver.find_elements(By.CLASS_NAME, 'accordion')
+    summaries = driver.find_elements(By.TAG_NAME, 'table')
+
+    # for title in class_titles:
+    #     print(title.text)
+
+    # for summary in summaries:
+    #     print(summary.text)
+
+    for title, summary in zip(class_titles, summaries):
+        print(f'Course: {title.text}\nTable: {summary.text}\n')
+
 service = Service(executable_path='/Users/eliorocha/Downloads/chromedriver-mac-arm64/chromedriver')
 driver = webdriver.Chrome(service=service)
 
 url = 'https://engineering.catholic.edu/academics/courses/course-schedules/index.html'
 driver.get(url)
+
+subjects = [
+    'Biomedical Engineering',
+    'Civil & Environmental Engineer',
+    'Computer Science',
+    'Data Analytics',
+    'Electrical Engineering',
+    'Engineering Management: O/C',
+    'Engineering, General',
+    'Materials Science and Engineering',
+    'Physics'
+]
 
 wait()
 WebDriverWait(driver, 5).until(
@@ -24,38 +72,8 @@ WebDriverWait(driver, 5).until(
 term = Select(driver.find_element(By.ID, 'term'))
 term.select_by_visible_text('Spring 2026')
 
-wait()
-WebDriverWait(driver, 5).until(
-    EC.presence_of_all_elements_located((By.ID, 'subject'))
-)
-subject = Select(driver.find_element(By.ID, 'subject'))
-subject.select_by_visible_text('Computer Science')
+for subject in subjects:
+    loop_through_subjects(subject)
 
-wait()
-WebDriverWait(driver, 5).until(
-    EC.element_to_be_clickable((By.ID, 'view_catalog'))
-)
-input = driver.find_element(By.ID, 'view_catalog')
-input.click()
-
-wait()
-WebDriverWait(driver, 5).until(
-    EC.element_to_be_clickable((By.ID, 'courseSchedExpandAll'))
-)
-expand = driver.find_element(By.ID, 'courseSchedExpandAll')
-expand.click()
-
-wait()
-class_titles = driver.find_elements(By.CLASS_NAME, 'accordion')
-summaries = driver.find_elements(By.TAG_NAME, 'table')
-
-# for title in class_titles:
-#     print(title.text)
-
-# for summary in summaries:
-#     print(summary.text)
-
-for title, summary in zip(class_titles, summaries):
-    print(f'Course: {title.text}\nTable: {summary.text}\n')
 
 driver.quit()
